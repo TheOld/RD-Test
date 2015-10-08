4987eec14afc814168ffdcb9dba8fede2c6b4da3
Leandro Rodrigues <leandro_rodrigues@live.com>
2015-10-08 14:38:29 -0300

    Test heroku
    Testing Heroku integration

--- rdtest/config/application.rb
+++ rdtest/config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Rdtest
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +24,7 @@ module Rdtest
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+      
+      config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
